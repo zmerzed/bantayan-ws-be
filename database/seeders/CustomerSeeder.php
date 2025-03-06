aaaea67e0b7b34439da2dac48e04f7417adf9886
remz <zmerzed@gmail.com>
2025-03-06 10:22:44 +0800

    updates

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use App\Models\Customer;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Faker\Factory as Faker;
 
 class CustomerSeeder extends Seeder
 {
